2379f24a07c61b56aa81c926c4b9b85d47d96396
Dominic Bosch <dominic.bosch.db@gmail.com>
2016-02-18 19:47:56 +0100

    Raspberry compatibility

--- README.md
+++ README.md
@@ -6,8 +6,20 @@ USB Interface ideas from [https://github.com/kostmo/pyrocket](https://github.com
 
 ## Installation
 
+### Latest NodeJS, USB library and GCC-4.7
+
+This section is only required if you have troubles when trying to install the required packages in the next section below.
+Check your gcc version `gcc -v`. If the output is lower than 4.7 you need to take measures such as these:
+
+	sudo apt-get install g++-4.7
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6 
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7 
+	sudo update-alternatives --config gcc
+
+### Install required packages
+
 	sudo apt-get install build-essential libudev-dev
-	npm install
+	npm install 		# (I had to execute twice. after first time there was an error)
 
 ## Usage
 
--- usb-missiles.js
+++ usb-missiles.js
@@ -2,7 +2,7 @@
 
 let usb = require('usb');
 
-// console.log(usb);
+console.log(usb);
 
 let arrKnownMissiles = [
 	// '6465:32801',
@@ -12,29 +12,33 @@ let arrKnownMissiles = [
 	'8483:4112'
 ];
 
-// Could also use built in function: usb.findByIds(vid, pid)
-let arrFoundMissiles = usb.getDeviceList().filter(function(d) {
-	let desc = d.deviceDescriptor;
-	let identifier = desc.idVendor+':'+desc.idProduct;
-	if(arrKnownMissiles.indexOf(identifier) > -1) {
-		console.log('Found Rocket Launcher! Trying to access it...')
-		try {
-			d.open();
+function getAvailableDevices() {
+	return usb.getDeviceList().filter(function(d) {
+		let desc = d.deviceDescriptor;
+		let identifier = desc.idVendor+':'+desc.idProduct;
+		if(arrKnownMissiles.indexOf(identifier) > -1) {
+			console.log('Found Rocket Launcher! Trying to access it...')
+			try {
+				d.open();
 
-			if(d.interfaces[0].isKernelDriverActive()) {
-				console.log('Reattaching Kernel driver');
-				d.interfaces[0].detachKernelDriver();
+				if(d.interfaces[0].isKernelDriverActive()) {
+					console.log('Reattaching Kernel driver');
+					d.interfaces[0].detachKernelDriver();
+				}
+				// d.interfaces[0].attachKernelDriver();
+				d.interfaces[0].claim();
+				return true;
+			} catch(err) {
+				console.error('Unable to connect Rocket Launcher! Maybe you should sudo!');
+				return false;
 			}
-			// d.interfaces[0].attachKernelDriver();
-			d.interfaces[0].claim();
-			return true;
-		} catch(err) {
-			console.error('Unable to connect Rocket Launcher! Maybe you should sudo!');
-			return false;
 		}
-	}
-	return false;
-});
+		return false;
+	});
+}
+
+// Could also use built in function: usb.findByIds(vid, pid)
+let arrFoundMissiles = getAvailableDevices();
 
 exports.getNumAvailable = function() {
 	return arrFoundMissiles.length;
@@ -46,8 +50,12 @@ function sendCommand(bstr) {
 	for(let i = 0; i < arrFoundMissiles.length; i++) {
 		// controlTransfer(bmRequestType, bRequest, wValue, wIndex, data_or_length, callback(error, data))
 		// arrFoundMissiles[i].controlTransfer(0x21, 0x09, b.length, 0x0200, b, function(err, data) {
-		arrFoundMissiles[i].controlTransfer(0x21, 0x09, b.length, 0x0200, b, function(err, data) {
-			// if(err) console.log('err', err);
+		arrFoundMissiles[i].controlTransfer(0x21, 0x09, 0, 0, b, function(err, data) {
+			if(err && err.errno === usb.LIBUSB_ERROR_NO_DEVICE){
+				console.error('DEVICE Hang up! Reconnecting...');
+				arrFoundMissiles = getAvailableDevices();
+			}
+			if(err) console.log('err', err.message);
 			// console.log('data', data);
 		})
 	}
