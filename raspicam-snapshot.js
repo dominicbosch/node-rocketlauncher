0a3019bedf3b7ab16a7f3421710bbb8a4f2014de
dominic <dominic.bosch.db@gmail.com>
2016-05-20 11:19:16 +0200

    bugfix: child_process exec interface was used like spawn...

--- raspicam-snapshot.js
+++ raspicam-snapshot.js
@@ -3,17 +3,22 @@
 
 let execute = require('child_process').exec;
 
-exports.shoot = (inChannel) => {
-	execute('raspistill', [
-			'--nopreview',
-			'--timeout', '0',
-			'--width', '1024',
-			'--height', '768',
-			'--hflip', '--vflip',
-			'-o', '-'
-		], (err, stdout, stderr) => {
+exports.shoot = (cb) => {
+	execute('raspistill -n -t 0 -w 1024 -h 768 -hf -vf -o -',
+		{ maxBuffer: 5*1024*1024 }, // 5MB
+		(err, stdout, stderr) => {
+			console.log(err);
+			console.log(stderr);
 			if(err) return console.error('exec error: ' + err);
-			stdout.pipe(inChannel);
+			cb(stdout)//.pipe(inChannel);
 		}
 	);
 }
+	// execute('raspistill', [
+	// 		'--nopreview',
+	// 		'--timeout', ' 0',
+	// 		'--width', ' 1024',
+	// 		'--height', '768',
+	// 		'--hflip', '--vflip',
+	// 		'-o', '-'
+	// 	]
